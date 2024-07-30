fdf16f01011d8b51e90d55584589fd63b9ec169f
Balint Mazzag <mazzag.balint@researchflow.hu>
2024-07-30 15:18:40 +0200

    adding read_xlsb() function

---
+++ R/read_xlsb_custom.R
@@ -0,0 +1,28 @@
+#' @title Reading xlsb files
+#'
+#' @description
+#' `read_xlsb()` Reads xlsb files with the help of odbc connections
+#'
+#' @param path String The relative or absolute path of the file. Finishes in ".xlsb"
+#' @param sheet String. Sheet in the xlsb document
+#' @returns A tibble
+#'
+#' @export
+
+
+read_xlsb <- function(path, sheet){
+  if(!endsWith(path, ".xlsb")){
+    stop("Path needs to end in .xlsb")
+  } else {
+  wb <- path # Give the file name
+  con2 <- RODBC::odbcConnectExcel2007(wb)
+  sheets <- stringr::str_remove_all(RODBC::sqlTables(con2)$TABLE_NAME, "[\\$\\']")
+
+  if(!(sheet %in% sheets)){
+    stop(paste0("Sheet name must in the file sheet name!", paste0(sheets, collapse = ", ")))
+  } else {
+  data <- RODBC::sqlFetch(con2, sheet) |> dplyr::tibble() # Provide name of sheet
+  return(data)
+  }
+  }
+}
