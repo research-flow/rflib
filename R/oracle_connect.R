703df46e05fea0ca9438e116821de3ff9c4489c0
Balint Mazzag <mazzag.balint@researchflow.hu>
2024-11-27 14:50:07 +0100

    oracle connect

--- R/oracle_connect.R
+++ R/oracle_connect.R
@@ -3,15 +3,13 @@
 #' @description
 #' `oracle_connect()` connects to RF schemes, with host, scheme name and password.
 #'
-#' @param scheme Character string. Existing scheme in the RF Oracle system
-#' @param pwd Character string. Password connected to the scheme.
-#' @param host Character string. Existing host where the RF Oracle system is run.
+#' @param schema Character string. Existing scheme in the RF Oracle system
 #' @param setLocale Boolean. If sys.setenv should be run tto set adequate Hungarian language options.
 #' @returns An ROracle connection OraConnection object.
 #'
 #' @export
 
-oracle_connect <- function(scheme, pwd, host, setLocale = F){
+oracle_connect <- function(schema, setLocale = T){
 
   if (setLocale){
     Sys.setenv(TZ = "CET")
@@ -20,24 +18,23 @@ oracle_connect <- function(scheme, pwd, host, setLocale = F){
   }
 
   drv <- DBI::dbDriver("Oracle")
-  host_val = host
-  port = 1521
-  svc = "DBRF_fra1zg.subnet03261150.vcn03261150.oraclevcn.com"
+  host_val = Sys.getenv("OCI_HOST")
+  port_val = Sys.getenv("OCI_PORT")
+  svc_val = Sys.getenv("OCI_SVC")
 
-  connect.string <- paste(
+  connect.string <- paste0(
     "(DESCRIPTION=",
-    "(ADDRESS=(PROTOCOL=tcp)(HOST=", host_val, ")(PORT=", port, "))",
-    "(CONNECT_DATA=(SERVICE_NAME=", svc, ")))", sep = "")
+    "(ADDRESS=(PROTOCOL=tcp)(HOST=", host_val, ")(PORT=", port_val, "))",
+    "(CONNECT_DATA=(SERVICE_NAME=", svc_val, ")))")
 
   if (!requireNamespace("ROracle", quietly = TRUE)){
     stop("Please install ROracle package!", call. = T)
   }
 
   ROracle::dbConnect(drv,
-                     username = scheme,
-                     password = pwd,
+                     username = schema,
+                     password = Sys.getenv(paste0("OCI_", schema, "_PW")),
                      dbname = connect.string,
                      encoding = "windows-1250"
   )
-
 }
