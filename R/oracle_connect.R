45c40f00d65da40dc4f479c6ff14f886ec4b39ea
Balint Mazzag <mazzag.balint@researchflow.hu>
2024-12-03 16:10:50 +0100

    new oracle_connect

--- R/oracle_connect.R
+++ R/oracle_connect.R
@@ -1,15 +1,19 @@
 #' @title Connect to RF Oracle schemes
 #'
+#' @importFrom odbc dbConnect
+#' @importFrom odbc odbc
+#'
 #' @description
 #' `oracle_connect()` connects to RF schemes, with host, scheme name and password.
 #'
 #' @param schema Character string. Existing scheme in the RF Oracle system
+#' @param driverName Character string. Driver name
 #' @param setLocale Boolean. If sys.setenv should be run tto set adequate Hungarian language options.
 #' @returns An ROracle connection OraConnection object.
 #'
 #' @export
 
-oracle_connect <- function(schema, setLocale = T){
+oracle_connect <- function(schema, driverName = "Oracle in instantclient_21_15", setLocale = T){
 
   if (setLocale){
     Sys.setenv(TZ = "CET")
@@ -17,7 +21,6 @@ oracle_connect <- function(schema, setLocale = T){
     Sys.setenv(ORA_SDTZ = "CET")
   }
 
-  drv <- DBI::dbDriver("Oracle")
   host_val = Sys.getenv("OCI_HOST")
   port_val = Sys.getenv("OCI_PORT")
   svc_val = Sys.getenv("OCI_SVC")
@@ -27,14 +30,12 @@ oracle_connect <- function(schema, setLocale = T){
     "(ADDRESS=(PROTOCOL=tcp)(HOST=", host_val, ")(PORT=", port_val, "))",
     "(CONNECT_DATA=(SERVICE_NAME=", svc_val, ")))")
 
-  if (!requireNamespace("ROracle", quietly = TRUE)){
-    stop("Please install ROracle package!", call. = T)
-  }
-
-  ROracle::dbConnect(drv,
-                     username = schema,
-                     password = Sys.getenv(paste0("OCI_", schema, "_PW")),
-                     dbname = connect.string,
-                     encoding = "windows-1250"
+  odbc::dbConnect(
+    odbc::odbc(),
+    Driver = driverName,
+    DBQ = connect_string,
+    UID = schema,
+    PWD = Sys.getenv(paste0("OCI_", schema, "_PW")),
+    encoding = "windows-1250"
   )
 }
